05c04fd0d6c3564bd479936f7a2a8327a5cef217
asifahmad1011 <asif.apag@gmail.com>
2022-09-14 11:01:54 +0200

    initial implementation of task

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,171 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <button @click="fetchData()">Fetch Data From API</button>
+
+    <table class="table table-striped table-hover">
+      <thead v-if="api_data.data">
+        <tr>
+          <!-- <th>id</th> -->
+          <th>Name</th>
+          <th>Address</th>
+          <th>Email</th>
+          <th>Company</th>
+          <th>Phone #</th>
+          <th>Website</th>
+          <!-- <th>Allignment</th> -->
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(person, index) in api_data.data" :key="index">
+          <!-- <td>{{ person.id }}</td> -->
+          <td>{{ person.name }}</td>
+          <td>{{ person.address.city }}</td>
+          <td>{{ person.email }}</td>
+          <td>{{ person.company.name }}</td>
+          <td>{{ person.phone }}</td>
+          <td>{{ person.website }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+  <br />
+
+    <button @click="postUser()">Register User</button>
+
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+  },
+  data() {
+    return {
+      api_data: []
+    }
+  },
+
+  methods: {
+    fetchData: async function () {
+      let headers = {
+        'accept': 'application/json',
+        'Authorization': 'Bearer OxBONbQaVU6zYG3G0QeN'
+      }
+      this.api_data = await axios.get(
+        'https://zeigsuns.bredex.de', { headers }
+
+      )
+      // .then(response => (this.api_data = response))
+
+      console.log(this.api_data)
+    },
+
+    postUser: async function () {
+      let testUser =
+      {
+        name: "Janice Romero",
+        username: "Janice123",
+        email: "janice.romero@example.com",
+        address: {
+          street: "Lindentwete 1",
+          suite: "Suite B",
+          city: "Braunschweig",
+          zipcode: "38100",
+          geo: {
+            lat: "52.2648197",
+            lng: "10.5180167"
+          }
+        },
+        phone: "+49 123 4567890",
+        website: "https://example.com",
+        company: {
+          name: "BREDEX GmbH",
+          catchPhrase: "Gemeinsam gestalten wir Ihren digitalen Wandel",
+          bs: "Maßgeschneiderte Software-Lösungen"
+        }
+      }
+      let headers = {
+        'accept': 'application/json',
+        'Authorization': 'Bearer OxBONbQaVU6zYG3G0QeN',
+        'Content-Type': 'application/json'
+      }
+
+      this.api_data = await axios.post(
+        'https://zeigsuns.bredex.de', { headers, testUser }
+
+      ).then(function (response) {
+        console.log(response);
+      })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
+
   }
+
+
+
+
+
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h1,
+h2 {
+  font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
+}
+
+table {
+  font-family: "Open Sans", sans-serif;
+  width: 1000px;
+  border-collapse: collapse;
+  border: 3px solid #44475c;
+  margin: 10px 10px 0 10px;
+}
+
+table th {
+  text-transform: uppercase;
+  text-align: left;
+  background: #44475c;
+  color: #fff;
+  padding: 8px;
+  min-width: 30px;
+}
+
+table td {
+  text-align: left;
+  padding: 8px;
+  border-right: 2px solid #7d82a8;
+}
+
+table td:last-child {
+  border-right: none;
+}
+
+table tbody tr:nth-child(2n) td {
+  background: #d4d8f9;
 }
 </style>
